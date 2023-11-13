d9d62407a6fe1beede2ae9995099009097d80173
PoH-rull <101949468+PoH-rull@users.noreply.github.com>
2023-11-13 19:08:02 +0200

    created binary tree

--- tree/src/Person.java
+++ tree/src/Person.java
@@ -1,13 +1,16 @@
 public class Person {
     private String name;
     private int age;
+    private String relation;
     private Person left;
     private Person right;
-    public Person(String name, int age){
+    public Person(String name, int age, String relation) {
         this.name = name;
         this.age = age;
+        this.relation = relation;
         this.left = null;
         this.right = null;
     }
+
 
 }
