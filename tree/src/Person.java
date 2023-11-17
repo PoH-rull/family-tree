7030d40429709474a633e7648e24f3c230ce08a0
gamingeveryday <lasovskiguy@gmail.com>
2023-11-18 01:33:09 +0200

    1.0
    comments

--- tree/src/Person.java
+++ tree/src/Person.java
@@ -1,11 +1,11 @@
 public class Person {
-    private String name;
-    private int age;
+    private String name;//name of this person
+    private int age;//age of this person
     private String relation;//based to on the person acting as root
-    private Person left;
-    private Person right;
+    private Person left;//person to the left of this person
+    private Person right;//person to the right of this person
 
-    public Person(String name, int age, String relation) {
+    public Person(String name, int age, String relation) {//we also need to make a constructor for a person without any relation (root)
         this.name = name;
         this.age = age;
         this.relation = relation;
@@ -13,21 +13,32 @@ public class Person {
         this.right = null;
     }
 
+    //get the person to the right of this person
     public Person getRight() {
         return right;
     }
 
+    //get the person to the left of this person
     public Person getLeft() {
         return left;
     }
+
+    //get the name of this person
     public String getName() {
         return name;
     }
 
+    //get the age of this person
     public int getAge() {
         return age;
     }
 
+    //get the relation of this person to the root
+    public String getRelation() {
+        return relation;
+    }
+
+    //return if this person is the same as the other person
     public boolean Equals(Person person){
         if(this.name.equals(person.getName()) && this.age == person.getAge()){
             return true;
@@ -35,17 +46,16 @@ public class Person {
         return false;
     }
 
+    //set the person to the right of this person
     public void setRight(Person person) {
         this.right = person;
     }
 
+    //set the person to the left of this person
     public void setLeft(Person person) {
         this.left = person;
     }
 
-    public String getRelation() {
-        return relation;
-    }
     public String toString(){
         return name + " " + age + " " + relation;
     }
